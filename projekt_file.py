2182fe702bef0b5da984a148c6199f92a2e99e11
jodarell <jo4531da-s@student.lu.se>
2025-05-14 14:15:33 +0200

    Add files via upload

---
+++ projekt_file.py
@@ -0,0 +1,14 @@
+### Projekt FMSF50 Matematisk statistik Maj 2025
+
+import pandas as pd
+
+luledf = pd.read_csv("luleälven_årsmax.csv")
+vindeldf = pd.read_csv("vindelälven_årsmax.csv")
+
+
+
+"""
+Fråga 1: Jämför grafiskt hur årsmaximum varierar under de olika perioderna för respektive älv.
+Presentera en översiktstabell med antal observationer, medelvärde, standardavvikelse, minimum och maximum i de fyra fallen
+"""
+
